8da91159219fefcfca03094b1ceab3d53a1d702c
IshikaB3003 <ishikabansal3003@gmail.com>
2022-12-02 03:06:14 +0530

    express added

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+
+
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello Ishika!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+// 1.First, write npm install --save nodemon then in package.json write the followings
+// 2."scripts": {
+//     "server": "nodemon server.js"
+//   },
+// 3.then write in  the terminal
+
+// npm run server
