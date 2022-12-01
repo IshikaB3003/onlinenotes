6a6d4263dc8eaa788a5adafa08300024e62f42bb
IshikaB3003 <ishikabansal3003@gmail.com>
2022-12-02 03:27:23 +0530

    models and schema added

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,12 @@ connectToMongo();
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello Ishika!')
-})
+//Available Routes
+// app.get('/', (req, res) => {
+//   res.send('Hello Ishika!')
+// })
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
